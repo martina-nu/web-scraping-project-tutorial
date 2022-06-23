7cd7296a87f9be9390eb59d577bee4ee0700bcc9
martina-nu <martinanu98@gmail.com>
2022-06-23 18:02:07 +0000

    Adding my solution

--- src/app.py
+++ src/app.py
@@ -36,7 +36,53 @@ for row in tables[my_index].tbody.find_all("tr"):
         rev = col[1].text.replace("$","").replace(",","")
         df = df.append({"Date":fecha, "Revenue":rev}, ignore_index=True)
 
-print(df)
+#print(df)
+
+
+#Step 5: Clean rows (In exercise.ipynb)
+
+df = df[df['Revenue'] != ""] #Filter empty strings out
+
+# Step 6: Insert the data into sqlite3
+
+# Insert the data into sqlite3 by converting the dataframe into a list of tuples
+
+list_tuples = list(df.itertuples(index=False, name=None))
+
+
+# Step 7: Connect to SQLite
+
+conn = sqlite3.connect('Tesla.db')
+
+
+# Step 8: Let's create a table in our database to store our revenue values
+
+c = conn.cursor()
+
+# Create table
+c.execute('''CREATE TABLE revenue
+              (Date, Revenue)''')
+
+# Insert values
+
+c.executemany('INSERT INTO revenue VALUES (?,?)', list_tuples)
+
+
+# Commit
+conn.commit()
+
+
+# Step 9: Retrieve data
+
+for row in c.execute('SELECT * FROM revenue'):
+    print(row)
+
+
+# Step 10: Finally create a plot to visualize the data (In exercise.ipynb)
+
+
+
+
 
 
 
